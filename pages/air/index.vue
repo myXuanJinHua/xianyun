601163b892f9d19ac9ac1c9b6a3d3d04f075b9d5
hua <a6927561@163.com>
2019-07-30 11:55:27 +0800

    第一次初始化项目,配置了nuxt的配置文件

---
+++ pages/air/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>airasdsfghfgjhkjlkhjgfds</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>首页</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="less" scoped>
+div{
+  color: #660300fd
+}
+</style>
